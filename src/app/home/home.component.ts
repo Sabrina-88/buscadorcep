146183cac30ee5a8d27832ea3b20c158ab8c6c17
Sabrina Andrade <madame_andrade@yahoo.com.br>
2019-09-11 23:56:06 -0300

    new validations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,17 +17,37 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
   search(){
-    this.resultContainer = true
     this.cepService.buscar(this.cep.cep)
-      .then((cep:Cep) => this.cep = cep)
+      .then((cep:Cep) => {
       
+        this.resultContainer = true
+    this.cep = cep;
+    
+        console.log('correto')
+      })
+    
       .catch(() => {
         this.resultContainer = false;
-        let inputCep = this.cep.cep;
-        
-        this.cep = new Cep()// limpar campo
-        this.cep.cep = inputCep;
-        alert('Não foi possivel realizar a busca.');
+    let inputCep = this.cep.cep;
+    let regex = /[a-zA-Z ]/g;
+    let result = inputCep.match(regex);
+    if (result){
+      alert('Digite somente números.');
+      console.log("voce digitou letra>>> ", result);
+    } else if   (inputCep.length <= 7){
+      alert("Atenção faltam digítos no campo CEP. Preencha com 8 dígitos");
+      // this.newSearch()
+    } 
+    else {
+         //this.cep = new Cep()// limpar campo
+        // this.cep.cep = inputCep;
+      alert('Não foi possível realizar a busca.'); 
+    } 
       })
+  }
+
+  newSearch(){
+    this.resultContainer = false;
+    this.cep.cep = '';
   }
 }
